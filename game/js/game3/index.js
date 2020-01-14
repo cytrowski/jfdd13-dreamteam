4e95eebe016f4e1be6340b5666ca1fc4e1e26d4d
Bartosz Cytrowski <bartosz.cytrowski@gmail.com>
2020-01-14 01:13:12 +0100

    Copy the scoreboard code

--- game/js/game3/index.js
+++ game/js/game3/index.js
@@ -1,4 +1,7 @@
 import { instruction } from "./instruction";
 import { gameIndex } from "./game-index";
+import {scoreboard} from './scoreboard/scoreboardLogic'
 instruction();
+scoreboard();
 gameIndex();
+
